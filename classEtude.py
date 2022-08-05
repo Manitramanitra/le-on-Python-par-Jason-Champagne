88dfb0d7378adfbf4ec5eca6ee26dfb7e89bb7f9
Manitramanitra <randiluc@gmail.com>
2022-08-05 15:11:42 +0300

    différent méthode d'une classe en python

--- classEtude.py
+++ classEtude.py
@@ -1,21 +1,34 @@
-#coding:utf-8
+# coding:utf-8  
 class Humain:
-    humain_creer = 0 
+    humain_creer = 0 # ceci est une attribut de classe ceci est liée à la classe qui appartient a tous les classes
+    lieu_habitation = "terre"
     """ 
     Classe des êtres humains
     """
-    def __init__(self,c_prenom,c_age):#self cible l'objet lui même
+
+    def __init__(self, c_prenom, c_age):  # mot clé 'self' cible l'objet lui même sur un intance
         print('création d\'un humain...')
         self.prenom = c_prenom
         self.age = c_age
-        Humain.humain_creer +=1
+        Humain.humain_creer += 1
+    def parler(self,message):
+        print("{} a dit: {} ".format(self.prenom,message))
+     
+    def changer_planete(cls,nouvelle_planete): # cls méthode de classe
+        Humain.lieu_habitation = nouvelle_planete  
+    changer_planete = classmethod(changer_planete)
 
+    def definition():
+        print("blablabla...")
+    definition = staticmethod(definition)
 print("Lancement du programme...")
 
-h1 = Humain("manitra",23)
-print("Humains existants: {}".format(Humain.humain_creer))
-h2 = Humain("Luc",22)
-print("Humains existants: {}".format(Humain.humain_creer))
+h1 = Humain("manitra", 23)
+print("Humains existants: {}".format(Humain.humain_creer)) 
 
+print("Planéte ancien: {} ".format(Humain.lieu_habitation))
 
- 
+Humain.changer_planete("Mars")
+print("Planéte actuelle: {} ".format(Humain.lieu_habitation))
+
+Humain.definition()
